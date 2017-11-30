40d4ac19aab2322a793c8e43f27fea535e4f2a8e
darkblank <darkblank1990@gmail.com>
2017-11-30 16:51:43 +0900

    imp_uid로 정보 불러오기 테스트

--- zinzi/reservations/payment.py
+++ zinzi/reservations/payment.py
@@ -2,5 +2,5 @@ from iamport import Iamport
 
 iamport = Iamport(imp_key='6343293486082258',
                   imp_secret='JEAB6oXOMsc2oysgdu4tJzlfgQvn5sfP7Qqefn21Qe3fNwv11zuL9Q0qGvNMY2B6T1l8pn9fCdvpK0rL')
-response = iamport.find(merchant_uid='merchant_1511853397921')
+response = iamport.find(imp_uid='imp_131520085919')
 print(response)
